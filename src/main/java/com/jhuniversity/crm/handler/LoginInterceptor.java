b66415c5449a5dd139a05b0696d7d7f07ca92dfe
zel <zel@qq.com>
2020-11-18 12:26:53 +0800

    crm_ssm_springMVC_first

--- src/main/java/com/jhuniversity/crm/handler/LoginInterceptor.java
+++ src/main/java/com/jhuniversity/crm/handler/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.jhuniversity.crm.handler;
 
+import com.jhuniversity.crm.settings.domian.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        
+        User user =(User)request.getSession().getAttribute("user");
+        if (user==null){
+
+        }
         return false;
     }
 
